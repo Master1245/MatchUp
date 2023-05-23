64c0cf5d2eefd3f3514d8567df69e4bea78763ff
Mateus L Silvano <mateus.l2002@aluno.ifsc.edu.br>
2023-05-22 21:06:36 -0300

    Ajustando parte de login
    Colocando parte de de login e logout

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
+from datetime import timedelta, datetime
+from jose import JWTError, jwt
 from fastapi import FastAPI, Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
-
 from api.dao.User import UserDao
 from db import crud, models, schemas
 from db.database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
-
 app = FastAPI()
+SECRET_KEY = "seu_secret_key_aqui"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 40
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
 
 
 def get_db():
@@ -23,14 +28,15 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.post("/users/", response_model=schemas.User)
+@app.post("/users/")
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
+    hashed_password = UserDao.get_password_hash(user.password)
+    user.password = hashed_password
     user_save = crud.create_user(db=db, user=user)
     user_json = UserDao.to_dict_api(user_save)
-    print(user_json)
     return user_json
 
 
@@ -42,3 +48,61 @@ def delete_user(email: str, db: Session = Depends(get_db)):
         return {"message": "User deleted"}
     else:
         raise HTTPException(status_code=400, detail="User not found")
+
+
+def get_user_by_email_or_username(db: Session, email_or_username: str):
+    user = crud.get_user_by_email(db, email_or_username)
+    if not user:
+        user = crud.get_user_by_username(db, email_or_username)
+    return user
+
+
+def authenticate_user(db: Session, email_or_username: str, password: str):
+    user = get_user_by_email_or_username(db, email_or_username)
+    if not user:
+        return False
+    if not UserDao.verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: timedelta):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + expires_delta
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+@app.post("/login", tags=["Authentication"])
+def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = authenticate_user(db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(status_code=401, detail="Incorrect email or password")
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.email}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@app.get("/users/me", tags=["Users"])
+def read_user_me(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email = payload.get("sub")
+        if email is None:
+            raise HTTPException(status_code=401, detail="Invalid authentication token")
+        db = SessionLocal()
+        user = crud.get_user_by_email(db, email=email)
+        db.close()
+        if user is None:
+            raise HTTPException(status_code=404, detail="User not found")
+        return UserDao.to_dict_api(user)
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid authentication token")
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
