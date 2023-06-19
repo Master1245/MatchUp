018eebbb88811ecc0c4d741c3a5f6a599c315fb7
Mateus L Silvano <mateus.l2002@aluno.ifsc.edu.br>
2023-06-19 19:16:07 -0300

    Ajustar parte de pegar o token

--- api/get.py
+++ api/get.py
@@ -1,7 +1,6 @@
-from typing import Optional, List
+from typing import Optional
 from jose import jwt, JWTError
 from sqlalchemy.orm import Session
-
 from api.auth import SECRET_KEY, ALGORITHM, oauth2_scheme, revoked_tokens
 from db import crud
 from db.database import SessionLocal, get_db
@@ -9,6 +8,29 @@ from db.models import User
 from fastapi import Depends, HTTPException, APIRouter, Query
 
 router = APIRouter()
+
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email = payload.get("sub")
+        if email is None:
+            raise HTTPException(
+                status_code=401,
+                detail="Invalid authentication token",
+            )
+        user = crud.get_user_by_email(db, email)
+        if user is None:
+            raise HTTPException(
+                status_code=401,
+                detail="User not found",
+            )
+        return user
+    except jwt.JWTError:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid authentication token",
+        )
 
 
 @router.get("/users/me", tags=["Users"])
